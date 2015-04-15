2b1f18fd771ad1b2b809ba11e2c49c6aedcb1c5f
Caspar0v <cbvanbeusekom@12move.nl>
2015-04-15 12:02:28 +0200

    Parse inschrijfavond

---
+++ main.js
@@ -0,0 +1,27 @@
+////////////////////////////////////
+// Inschrijf avond KVW            //
+////////////////////////////////////
+Parse.initialize("4pAG568039x8dBBuekW2JvZhZahlImuqWpXHjums", "NlLcCGaYN7QGVzQUScXcRjj26QnVJT6O8iUU1mJH");
+    
+var inschrijfDb = Parse.Object.extend("inschrijfDb");
+var data = new inschrijfDb();
+
+var bank = new Object();
+
+$(document).ready(function(){
+	bank = new Object();
+	bank.name = "name"
+});
+
+$(document).ready(function(){
+	$("#submit").on('click', function(){
+		data.save(bank, {
+		   	success: function(object) {
+		   		alert("Succes");
+		  	},
+	   		error: function(model, error) {
+	       		alert("Error");
+	   		}
+		})
+	})
+});
