6ee0de75dbbc4210fc1eba6062c3193b33fd2f63
Caspar0v <cbvanbeusekom@12move.nl>
2015-04-22 20:47:52 +0200

    Send

--- main.js
+++ main.js
@@ -13,14 +13,14 @@ $(document).ready(function(){
 
 $(document).ready(function(){
 	$("#submit").on('click', function(){
-		var answer1 = confirm("Wilt u nog meer kinderen inschrijven ?");
+		var answer1 = confirm("Wilt u nog meer kinderen inschrijven ? 
+			Zo ja, dan worden de basis gegevens gekopieërd. (e-mail, telefoon, etc)");
 		if(answer1){
 			var answer2 = confirm("Weet u zeker dat alle gegevens kloppen ?");
 			if(antwoord) {
 				data.save(temporaryBank, {
 			   		success: function(object) {
 			   			alert("Alle gegevens zijn opgeslagen, bedankt.");
-			   			window.location.reload(true);
 					},
 		   			error: function(model, error) {
 		       			alert("Error");
