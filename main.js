694c8c670450258d059a6f2ef08332fee44aedd6
Caspar0v <cbvanbeusekom@12move.nl>
2015-04-22 17:32:50 +0200

    bump

--- main.js
+++ main.js
@@ -5,22 +5,42 @@ Parse.initialize("4pAG568039x8dBBuekW2JvZhZahlImuqWpXHjums", "NlLcCGaYN7QGVzQUSc
     
 var inschrijfDb = Parse.Object.extend("inschrijfDb");
 var data = new inschrijfDb();
-var bank = new Object();
+var temporaryBank = new Object();
 
 $(document).ready(function(){
-	bank = new Object();
-	
+	temporaryBank = new Object();
 });
 
 $(document).ready(function(){
 	$("#submit").on('click', function(){
-		data.save(bank, {
-		   	success: function(object) {
-		   		alert("Succes");
-		  	},
-	   		error: function(model, error) {
-	       		alert("Error");
-	   		}
-		})
+		var answer1 = confirm("Wilt u nog meer kinderen inschrijven ?");
+		if(answer1){
+			var answer2 = confirm("Weet u zeker dat alle gegevens kloppen ?");
+			if(antwoord) {
+				data.save(bank, {
+			   		success: function(object) {
+			   			alert("Alle gegevens zijn opgeslagen, bedankt.");
+			   			window.location.reload(true);
+					},
+		   			error: function(model, error) {
+		       			alert("Error");
+		   			}
+				})
+			};
+		}
+		else {
+			var answer2 = confirm("Weet u zeker dat alle gegevens kloppen ?");
+			if(antwoord) {
+				data.save(bank, {
+			   		success: function(object) {
+			   			alert("Alle gegevens zijn opgeslagen, bedankt.");
+			   			window.location.reload(true);
+					},
+		   			error: function(model, error) {
+		       			alert("Error");
+		   			}
+				})
+			};
+		}
 	})
 });
